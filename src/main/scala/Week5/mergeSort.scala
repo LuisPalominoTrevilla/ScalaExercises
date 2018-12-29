38e7bfb6e1952d81d213abf13f734c63c4f91ce7
Luis Palomino Trevilla <luispalominot3@gmail.com>
2018-12-28 18:19:21 -0600

    Implicit parameter on mergesort

--- src/main/scala/Week5/mergeSort.scala
+++ src/main/scala/Week5/mergeSort.scala
@@ -1,16 +1,17 @@
 package Week5
+import math.Ordering
 
 object mergeSort extends App {
-  def msort(xs: List[Int]): List[Int] = {
+  def msort[T](xs: List[T])(implicit ord: Ordering[T]): List[T] = {
     val n = xs.length/2
     if (n == 0) xs
     else {
-      def merge(xs: List[Int], ys: List[Int]): List[Int] =
+      def merge(xs: List[T], ys: List[T]): List[T] =
         (xs, ys) match {
           case (Nil, ys) => ys
           case (xs, Nil) => xs
           case (x :: xs1, y :: ys1) =>
-            if (x < y) x :: merge(xs1, ys)
+            if (ord.lt(x, y)) x :: merge(xs1, ys)
             else y :: merge(xs, ys1)
         }
       val (left, right) = xs splitAt n
@@ -18,6 +19,8 @@ object mergeSort extends App {
     }
   }
 
-  val list = List(2,4,5,1,2,9,10,23,2,5,1,-1,0,234,-102,20,-23,123,2,5,67,490,12,-10)
-  println(msort(list))
+  val numbers = List(2,4,5,1,2,9,10,23,2,5,1,-1,0,234,-102,20,-23,123,2,5,67,490,12,-10)
+  val fruits = List("apple", "pineapple", "orange", "banana")
+  println(msort(numbers))
+  println(msort(fruits))
 }
